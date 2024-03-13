35102b45be837ba9851365d2fd163ad599f0b195
Natalia Sobisz <nata.so@wp.pl>
2024-03-13 16:28:11 +0100

    sentry błędy

--- testproject/urls.py
+++ testproject/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-"""
+
 def division_by_zero(request):
    division_by_zero = 1 / 0
    
@@ -41,10 +41,10 @@ def no_lib(request):
 
 
 
-"""
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sentry-debug/',),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
